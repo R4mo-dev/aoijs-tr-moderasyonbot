414d2481158e17931d2962960a325d39bacf8749
R4mo-dev <121640965+R4mo-dev@users.noreply.github.com>
2023-11-24 21:59:37 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+  name: "ban",
+  code: `
+$yasakla[$sunucuID;$mentioned[1]]
+$tanimlama[<@$mentioned[1]> Kullanıcısı Sunucudan Başarıyla Yasaklandı.]
+$onlyIf[$noMentionMessage!=;{newEmbed:{title:Hata!}{description:Bir Sebep Girmelisin}{color:#FF0000}}]
+$onlyIf[$mentioned[1]!=;{newEmbed:{title:Hata!}{description:Yasaklamak İstediğin Kullanıcıyı Etiketle!}{color:#FF0000}}]
+$onlyPerms[banmembers;{newEmbed:{title:Hata!}{description:Bu Komutu Kullanabilmek İçin **Üyeleri Yasakla** Yetkisine Sahip Olmalısın!}{color:#FF0000}}]
+  `
+}
